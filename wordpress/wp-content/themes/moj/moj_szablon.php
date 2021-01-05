b5a511510e4425c301cd689512b601c0296f4776
szariii <76693916+szariii@users.noreply.github.com>
2021-01-05 10:29:50 +0100

    Add files via upload

--- wordpress/wp-content/themes/moj/moj_szablon.php
+++ wordpress/wp-content/themes/moj/moj_szablon.php
@@ -1,14 +1,17 @@
 <?php /* Template Name: moj_szablon */ ?>
 <?php get_header();?>
 <div class="container">
+
 <div class="row">
+
 <div class="col-md-6"> 
+
 <h1 style="text-align:center">Formularz</h1>
 <h2> E-mail: </h2>
 <input type="text" id="email" name="email">
 <h2> Hasło: </h2> 
 <input type="text" id="haslo" name="haslo"> 
-<div>
+</div>
 </div>
 </div>
 
