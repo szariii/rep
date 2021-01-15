20fdec3ab284bdfdcf096d24af6cff48daea74f7
Adrian Kulig <adrian.kulig@quantoo.pl>
2021-01-15 16:43:46 +0100

    poprawienie kodu

--- wordpress/wp-content/themes/moj/functions.php
+++ wordpress/wp-content/themes/moj/functions.php
@@ -1,21 +1,37 @@
 <?php
 
-function custom_theme_assets() {
-	wp_enqueue_style('style',get_stylesheet_uri() );
-	
-	#wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true  );
-	
-	wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/przycisk.js', array('jquery'), null, true  );
-	
-	#wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/dane.js', array('jquery'), null, true  );
-	
+function custom_theme_assets()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
 
+    #wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true  );
+
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/przycisk.js', array('jquery'), null, true);
 }
 
-add_action('wp_enqueue_scripts','custom_theme_assets');
+add_action('wp_enqueue_scripts', 'custom_theme_assets');
 
 
-add_action( 'wp_ajax_dane', 'dane' );
-add_action( 'wp_ajax_nopriv_dane', 'dane');
+add_action('wp_ajax_dane', 'dane');
+add_action('wp_ajax_nopriv_dane', 'dane');
+function dane()
+{
+    // pobierz dane przekazane metoda POST z Javscript Ajax;
+    $data = $_POST;
+
+    // wyswietl dane
+    var_dump($data);
+
+    // wywolaj funkcje zapisujaca uzytkownika
+//    storeUser($data);
+}
+
+
+function storeUser($data)
+{
+    global $wpdb;
+    // zapisz dane do bazy uzyj $wpdb->insert()
+//    $wpdb->insert();
+}
 
 ?>
--- wordpress/wp-content/themes/moj/moj_szablon.php
+++ wordpress/wp-content/themes/moj/moj_szablon.php
@@ -1,24 +1,29 @@
 <?php /* Template Name: moj_szablon */ ?>
 
-<?php get_header();?>
-<form>
-<div class="container">
-<div class="row">
-<div class="col-md-6"> 
-<h1 style="text-align:center">Formularz</h1>
-<h2> E-mail: </h2>
-<input type="text" id="email" name="email">
+<?php get_header(); ?>
 
-<h2> Hasło: </h2> 
-<input type="text" id="haslo" name="haslo"> 
-<h6 style="font-size:xx-small">Nigdy nie udostepniaj hasła innym osobom</h6>
-<br>
-<input type="checkbox" class="form-check-input">
-<label>Zapamiętaj mnie</label>
-<br>
-<button onclick="dane" type="zat" class="btn btn-primary" id="prz">Zatwierdź</button>
-</div>
-</div>
-</div>
-</form>
-<?php get_footer();?>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 style="text-align:center">Formularz</h1>
+                    <h2> E-mail: </h2>
+                    <input type="text" id="email" name="email">
+                </div>
+                <div class="col-md-6">
+                    <div class="col-md-6">
+                        <h2> Hasło: </h2>
+                        <input type="password" id="haslo" name="haslo">
+                        <h6 style="font-size:xx-small">Nigdy nie udostepniaj hasła innym osobom</h6>
+                    </div>
+                </div>
+
+                <div class="col-md-12 text-center">
+                    <button type="button" class="btn btn-primary" id="prz">Zatwierdź</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+<?php get_footer(); ?>
